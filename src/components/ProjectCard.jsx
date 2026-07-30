6fe03e5bb7be8065e06f7021614826650607545e
Jason Chen <jasonchen4657@gmail.com>
2026-07-30 14:08:23 -0400

    style: refine CSS formatting for consistency; update class names in ProjectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ export default function ProjectCard(props) {
         <button
             key={tag}
             type="button"
-            className={"tag" + (props.darkMode ? " dark" : " light")}
+            className={props.darkMode ? "dark" : "light"}
             onClick={() => props.setFilter(tag)}
         >
             {tag}
@@ -16,24 +16,24 @@ export default function ProjectCard(props) {
             <div className={"project" + (props.darkMode ? " dark" : " light")}>
                 <h3>{props.title}</h3>
                 <p>
-                    {((props["start-date"].getYear() === props["end-date"].getYear()) && (props["start-date"].getMonth() !== props["end-date"].getMonth()) 
+                    {((props["start-date"].getYear() === props["end-date"].getYear()) && (props["start-date"].getMonth() !== props["end-date"].getMonth())
                         ? props["start-date"].toLocaleDateString("en-US", {
                             month: "long",
-                        }) 
+                        })
                         : props["start-date"].toLocaleDateString("en-US", {
                             month: "long",
                             year: "numeric"
                         })
-                    ) + ((props["start-date"].getTime() === props["end-date"].getTime()) 
-                        ? "" 
+                    ) + ((props["start-date"].getTime() === props["end-date"].getTime())
+                        ? ""
                         : " to " + props["end-date"].toLocaleDateString("en-US", {
                             month: "long",
                             year: "numeric"
                         })
-                    )}
+                        )}
                 </p>
                 <p>{props.description}</p>
-                <div className="tags">
+                <div className="filter-buttons">
                     {tags}
                 </div>
             </div>
